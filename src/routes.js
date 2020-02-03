a383b22aef45f53b5dae154db3df9c1026f72d03
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-03 13:01:05 -0300

    Cadastro de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 // Utilizando o sucrase para fazer as importações
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rafael Souza',
-    email: 'devrafaelsouza@gmail.com',
-    password_hash: '123123',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
