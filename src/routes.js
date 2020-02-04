329467c2fd4b39e9e7d78865df14e6fd573701d7
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-04 11:52:39 -0300

    autenticação via JWT criada

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 // Utilizando o sucrase para fazer as importações
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
