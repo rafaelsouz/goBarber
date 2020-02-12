223cf54e775319d4ab106810ff14c5646e20f0d8
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-11 21:48:14 -0300

    Validação dos dados do agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,13 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    return res.json();
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -26,10 +31,33 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers ' });
     }
 
+    // parseISO transforma em Objeto data do JS
+    // startOfHour despreza os minutos e segundos, e retorna apenas da hora. 18h35 fica apenas 18h.
+    const hourStart = startOfHour(parseISO(date));
+
+    // Verificando se o horario do agendamento já passou.
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    // Verificando se a data está disponivel pelo provedor
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
