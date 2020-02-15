1941ca2ec0e00f066cad79957cc9033de09ecc46
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-15 10:38:03 -0300

    Configurando fila de email com redis

---
+++ src/lib/Queue.js
@@ -0,0 +1,46 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+// Sempre que tivemos um novo jobs para ser executado adicionamos neste array
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    // Cada serviço deve ter sua fila propria.
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    // Desestruturando para ter acesso aos metodos key e handle de cada job
+    jobs.forEach(({ key, handle }) => {
+      // adicionando os jobs na variavels this.queues, usando as configuralçoes
+      // pro redis e iniciando os handle de cada job
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  // Metodo para adiconar novos job na fila esquecifica
+  // obs: Podemos ter varias filas para serviços diferentes
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  // Processando o handle da fila
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
---
+++ src/queue.js
@@ -0,0 +1,6 @@
+// Este arquivo vai ser executado separado da aplicação, pois a nossa fila pode está em outro servidor
+// E dessa forma a fila nunca vai influencia o restante da nossa aplicação
+
+import Queue from './lib/Queue';
+
+Queue.processQueue();
